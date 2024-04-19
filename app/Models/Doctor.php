d8c039f59b8f875feb0c8f097dad887c2d14227a
MyUsername <figusgiacomo04@gmail.com>
2024-04-19 17:55:44 +0200

    feat: Doctor Model relation w/Specialization; doctor_specialization;

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Doctor extends Model
 {
@@ -22,6 +23,10 @@ class Doctor extends Model
 
     public function user(): BelongsTo{
         return $this->belongsTo(User::class);
+    }
+
+    public function specialization(): BelongsToMany{
+        return $this->belongsToMany(Specialization::class);
     }
 
 }
