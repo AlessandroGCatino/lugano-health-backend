a520eeac11d43955c593d780931e27657ef7e577
MyUsername <figusgiacomo04@gmail.com>
2024-04-22 12:08:19 +0200

    feat: specialization seeder; Slug generate

--- database/seeders/SpecializationSeeder.php
+++ database/seeders/SpecializationSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\Specialization;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class SpecializationSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+                
+        $specializations = [
+            'Cardiologia',
+            'Dermatologia',
+            'Ginecologia',
+            'Neurologia',
+            'Oncologia',
+            'Ortopedia',
+            'Pediatria',
+            'Psichiatria',
+            'Endocrinologia',
+            'Oculistica',
+            'Chirurgia generale',
+            'Otorinolaringoiatria',
+        ];
+        
+        foreach($specializations as $element){
+            $newSpecialization = new Specialization();
+            $newSpecialization->name = $element;
+            $newSpecialization->slug = Str::slug($newSpecialization->name, '-');
+            $newSpecialization->save();
+        }
     }
 }
