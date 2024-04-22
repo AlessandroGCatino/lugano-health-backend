b1affab0805496323a99263d3b699513baba057b
MyUsername <figusgiacomo04@gmail.com>
2024-04-22 12:16:44 +0200

    feat : Vote Seeder;

--- database/seeders/VoteSeeder.php
+++ database/seeders/VoteSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Vote;
 
 class VoteSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class VoteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $starRatings = [
+            ['stars' => 1, 'description' => 'Pessimo'],
+            ['stars' => 2, 'description' => 'Scarso'],
+            ['stars' => 3, 'description' => 'Mediocre'],
+            ['stars' => 4, 'description' => 'Buono'],
+            ['stars' => 5, 'description' => 'Eccellente'],
+        ];
+
+        foreach ($starRatings as $element) {
+            Vote::create($element);
+        }
+        
     }
 }
