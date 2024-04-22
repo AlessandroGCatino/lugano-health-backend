27362f204051272cc4ae7559e618e930dd7c3fc2
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-04-22 13:10:26 +0200

    pages index e show

--- resources/views/pages/reviews/index.blade.php
+++ resources/views/pages/reviews/index.blade.php
@@ -2,7 +2,30 @@
 
 @section('content')
 
+<h1 class="mt-2 fw-bold">The reviews:</h1>
 
-         <h1>ciao</h1>
+{{-- <a href="{{route('pages.reviews.create')}}" class="btn btn-primary d-block ms-auto">Add A New review</a>
 
+<table class="table table-striped mt-4">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">review</th>
+        <th scope="col">Slug</th>
+      </tr>
+    </thead>
+    <tbody>
+
+        @foreach ( $reviews as $item )
+        <tr>
+            <th>{{$item->id}}</th>
+            <td><a href="{{route('pages.reviews.show', $item->slug)}}">{{$item->name}}</a></td>
+            <td>{{$item->slug}}</td>
+          </tr>
+        @endforeach
+
+    </tbody>
+  </table> --}}
+</div>
 @endsection
+
--- resources/views/pages/specializations/show.blade.php
+++ resources/views/pages/specializations/show.blade.php
@@ -2,7 +2,21 @@
 
 @section('content')
     <div class="container">
-      <H2>SHOW</H2>
+        <h2>questa è la show</h2>
+        {{-- <h1 class="mt-2 fw-bold">{{ $specializations->name }}</h1>
 
+        <div class="d-flex gap-2 justify-content-end">
+            <a href="{{ route('pages.specializations.edit', $specializations->slug) }}" class="btn btn-warning">EDIT</a>
+
+            <form action="{{ route('pages.specializations.destroy', $specializations->slug) }}" method="POST">
+
+                @csrf
+                @method('DELETE')
+
+                <button type="submit" class="btn btn-danger">
+                    DELETE
+                </button>
+            </form>
+        </div> --}}
     </div>
-@endsection
+
