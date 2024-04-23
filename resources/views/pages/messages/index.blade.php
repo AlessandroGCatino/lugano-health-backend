0a7e386366b0f6a9f42dc0b67006cc2a81d5815d
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-04-23 12:47:25 +0200

    index messages

--- resources/views/pages/messages/index.blade.php
+++ resources/views/pages/messages/index.blade.php
@@ -1,30 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mt-2 fw-bold">The messages:</h1>
+    <div class="container">
+        <h1 class="mt-4 mb-3 fw-bold">Messages Received</h1>
 
-    <a href="{{ route('pages.messages.create') }}" class="btn btn-primary d-block ms-auto">write a new message</a>
-
-    <table class="table table-striped mt-4">
-        <thead>
-            <tr>
-                <th scope="col">usernames</th>
-                <th scope="col">message</th>
-                <th scope="col">testi_messaggi</th>
-                <th scope="col">emails</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @foreach ($messages as $item)
-                <tr>
-                    <th>{{ $item->usernames }}</th>
-                    <td><a href="{{ route('pages.messages.show') }}">{{ $item->testi_messaggi }}</a></td>
-                    <th>{{ $item->emails }}</th>
-                </tr>
-            @endforeach
-
-        </tbody>
-    </table>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">From</th>
+                                        <th scope="col">Dates</th>
+                                        <th scope="col">Emails</th>
+                                        <th scope="col">Messages</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($messages->sortByDesc('date_sent') as $message)
+                                        <tr>
+                                            <td>{{ $message->user_name }}</td>
+                                            <td>{{ $message->date_sent }}</td>
+                                            <td>{{ $message->user_mail }}</td>
+                                            <td>{{ $message->message }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
