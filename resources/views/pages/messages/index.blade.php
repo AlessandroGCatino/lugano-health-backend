4b6e03a37381dea01ec792111d12b4ea9a8b5963
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-04-23 12:25:59 +0200

    index messages

--- resources/views/pages/messages/index.blade.php
+++ resources/views/pages/messages/index.blade.php
@@ -1,31 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
+    <h1 class="mt-2 fw-bold">The messages:</h1>
 
-<h1 class="mt-2 fw-bold">The messages:</h1>
+    <a href="{{ route('pages.messages.create') }}" class="btn btn-primary d-block ms-auto">write a new message</a>
 
-{{-- <a href="{{route('pages.messages.create')}}" class="btn btn-primary d-block ms-auto">Add A New message</a>
+    <table class="table table-striped mt-4">
+        <thead>
+            <tr>
+                <th scope="col">usernames</th>
+                <th scope="col">message</th>
+                <th scope="col">testi_messaggi</th>
+                <th scope="col">emails</th>
+            </tr>
+        </thead>
+        <tbody>
 
-<table class="table table-striped mt-4">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">message</th>
-        <th scope="col">Slug</th>
-      </tr>
-    </thead>
-    <tbody>
+            @foreach ($messages as $item)
+                <tr>
+                    <th>{{ $item->usernames }}</th>
+                    <td><a href="{{ route('pages.messages.show') }}">{{ $item->testi_messaggi }}</a></td>
+                    <th>{{ $item->emails }}</th>
+                </tr>
+            @endforeach
 
-        @foreach ( $messages as $item )
-        <tr>
-            <th>{{$item->id}}</th>
-            <td><a href="{{route('pages.messages.show', $item->slug)}}">{{$item->name}}</a></td>
-            <td>{{$item->slug}}</td>
-          </tr>
-        @endforeach
-
-    </tbody>
-  </table> --}}
-</div>
+        </tbody>
+    </table>
+    </div>
 @endsection
-
