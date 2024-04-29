53aefa46440bc2b3f5979c7a67b10094a86631a7
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-04-29 11:55:19 +0200

    show doctor

--- resources/views/pages/doctors/show.blade.php
+++ resources/views/pages/doctors/show.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
 
-@section('title','Il tuo profilo')
+@section('title', 'Your Profile')
 
 @section('content')
-<div class="container">
-    <h2>This is the profile of Dr. {{ $doctor->name }} {{ $doctor->surname }}</h2>
-
     <div class="container">
-        <div class="card">
-            <div class="card-header">
-                Doctor Details
+        <h2>This is the profile of Dr. {{ $doctor->name }} {{ $doctor->surname }}</h2>
+
+        <a href="{{ route('pages.doctors.edit', $doctor->slug) }}" class="btn btn-warning">EDIT</a>
+
+        <div class="container">
+            <div class="card">
+                <div class="card-header">
+                    Doctor Details
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Address: {{ $doctor->address }}</p>
+                    <p class="card-text">Phone Number: {{ $doctor->phone_number }}</p>
+                </div>
             </div>
-            <div class="card-body">
-                <p class="card-text">Address: {{ $doctor->address }}</p>
-                <p class="card-text">Phone Number: {{ $doctor->phone_number }}</p>
-            </div>
+            <form action="{{ route('pages.doctors.destroy', $doctor->slug) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+                    DELETE
+                </button>
+            </form>
         </div>
     </div>
-</div>
-
-
 @endsection
