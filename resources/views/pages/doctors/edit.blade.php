6a1f11f6092ff95c75c431d06e54a712f6126cc8
MyUsername <figusgiacomo04@gmail.com>
2024-04-30 12:57:14 +0200

    changed from 'edit' to 'modifica'

--- resources/views/pages/doctors/edit.blade.php
+++ resources/views/pages/doctors/edit.blade.php
@@ -242,7 +242,7 @@
                     <div class="mb-4 row mb-0 mt-4">
                         <div class="col-md-6 offset-md-4">
                             <button type="submit" class="btn btn-primary">
-                                {{ __('Edit') }}
+                                {{ __('Modifica') }}
                             </button>
                         </div>
                     </div>
