d0b088c155e08a1e8634abc2ecb39fef581ab13a
GiacomoMilitello <g.militello95@gmail.com>
2024-05-06 10:48:23 +0200

    solo per ema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,89 +79,10 @@
     </nav>
 </div>
 
-    <div id="app">
+<main class="">
+    @yield('content')
+</main>
 
-        <nav class="navbar navbar-expand-md navbar-light">
-            <div class="container">
-                <a class="navbar-brand d-flex align-items-center">
-                    <a href="{{ route('dashboard') }}">
-
-                        <div class="logo_laravel w-25">
-                            <img src="/lugano-health-logo-1.png" href="{{ route('dashboard') }}" alt="..."
-                                class="img-fluid ">
-                        </div>
-                    </a>
-                    <a id="app-title" href="{{ url('http://localhost:5174/') }}">LUGANO HEALTH</a>
-                    {{-- config('app.name', 'Laravel') --}}
-
-                </a>
-
-                <button class="navbar-toggler p-1" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width='1em' height='1em'
-                        fill='white'>
-                        <path
-                            d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-                    </svg>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                        <li id='home-route' class="nav-item mt-3 mt-md-0 text-center ms-md-3 text-white ms-0">
-                            <a href="{{ route('testApi') }}">{{ __('ApiTest') }}</a>
-                        </li>
-                        <li>
-                            <a class="nav-item mt-3 mt-md-0 text-center ms-md-3 text-white ms-0"
-                                href="{{ route('token') }}">Test Pagamento</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item mt-3 mt-md-0 text-center ms-md-3 ms-0">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item mt-3 mt-md-0 text-center mb-3 mb-md-0 ms-md-3 ms-0">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->doctor->name }} {{ Auth::user()->doctor->surname }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
-                                    <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="">
-            @yield('content')
-        </main>
-    </div>
 </body>
 
 </html>
