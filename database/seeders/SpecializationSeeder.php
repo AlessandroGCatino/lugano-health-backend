f9b10849b888cb833219e288ab1eae47b747100b
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-05-08 04:52:25 +0200

    creazione di tutti i seeder e correzione api dottori per inserire badge

--- database/seeders/SpecializationSeeder.php
+++ database/seeders/SpecializationSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Specialization;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class SpecializationSeeder extends Seeder
@@ -33,5 +34,43 @@ class SpecializationSeeder extends Seeder
             $newSpecialization->slug = Str::slug($newSpecialization->name, '-');
             $newSpecialization->save();
         }
+
+        $pivotTableData = [
+            ['doctor_id' => 1, 'specialization_id' => 1],
+            ['doctor_id' => 1, 'specialization_id' => 2],
+            ['doctor_id' => 2, 'specialization_id' => 2],
+            ['doctor_id' => 3, 'specialization_id' => 3],
+            ['doctor_id' => 4, 'specialization_id' => 1],
+            ['doctor_id' => 4, 'specialization_id' => 2],
+            ['doctor_id' => 5, 'specialization_id' => 4],
+            ['doctor_id' => 5, 'specialization_id' => 1],
+            ['doctor_id' => 6, 'specialization_id' => 1],
+            ['doctor_id' => 6, 'specialization_id' => 2],
+            ['doctor_id' => 6, 'specialization_id' => 5],
+            ['doctor_id' => 6, 'specialization_id' => 6],
+            ['doctor_id' => 7, 'specialization_id' => 1],
+            ['doctor_id' => 8, 'specialization_id' => 7],
+            ['doctor_id' => 9, 'specialization_id' => 10],
+            ['doctor_id' => 1, 'specialization_id' => 12],
+            ['doctor_id' => 1, 'specialization_id' => 11],
+            ['doctor_id' => 10, 'specialization_id' => 2],
+            ['doctor_id' => 11, 'specialization_id' => 1],
+            ['doctor_id' => 12, 'specialization_id' => 2],
+            ['doctor_id' => 13, 'specialization_id' => 1],
+            ['doctor_id' => 14, 'specialization_id' => 2],
+            ['doctor_id' => 15, 'specialization_id' => 1],
+            ['doctor_id' => 16, 'specialization_id' => 2],
+            ['doctor_id' => 17, 'specialization_id' => 1],
+            ['doctor_id' => 18, 'specialization_id' => 2],
+            ['doctor_id' => 19, 'specialization_id' => 1],
+            ['doctor_id' => 20, 'specialization_id' => 2],
+            ['doctor_id' => 17, 'specialization_id' => 8],
+            ['doctor_id' => 18, 'specialization_id' => 9],
+            ['doctor_id' => 19, 'specialization_id' => 5],
+            ['doctor_id' => 20, 'specialization_id' => 4],
+            
+        ];
+
+        DB::table('doctor_specialization')->insert($pivotTableData);
     }
 }
